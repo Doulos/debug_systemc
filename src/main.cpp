43c6b08430cc0c88459b0922262ef68e3e6da8ae
David C Black <david.black@doulos.com>
2023-05-10 09:35:27 -0500

    Count messages

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include "top.hpp"
 #include "timer.hpp"
+#include <string>
+using namespace std::literals;
 
 std::unique_ptr<Timer> timer;
 
 [[maybe_unused]] int sc_main( [[maybe_unused]] int argc, [[maybe_unused]] char* argv[] )
 {
+  constexpr const char* mesgType = "/Doulos/debugging_systemc/main";
   timer = std::make_unique<Timer>("app");
 
   Top top { "top" };
@@ -17,6 +20,12 @@ std::unique_ptr<Timer> timer;
 
   auto errors = sc_core::sc_report_handler::get_count( sc_core::SC_ERROR )
               + sc_core::sc_report_handler::get_count( sc_core::SC_FATAL );
+
+  auto message_count = sc_core::sc_report_handler::get_count( sc_core::SC_INFO )
+              + sc_core::sc_report_handler::get_count( sc_core::SC_WARNING )
+              + errors + 1;
+  SC_REPORT_INFO_VERB( mesgType, ( "Total messages: "s + std::to_string(message_count) ).c_str(), SC_NONE );
+
 
   return ( errors == 0 ? 0 : 1 );
 }
