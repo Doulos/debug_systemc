416cd859e4b28a0a0d104978e088da560ee23b86
David C Black <david.black@doulos.com>
2023-05-28 13:20:25 -0500

    Synchronize with syscfund/include
    Signed-off-by: David C Black <david.black@doulos.com>

--- debug/report.hpp
+++ debug/report.hpp
@@ -3,32 +3,35 @@
 #include <systemc>
 #include <string>
 
+// This version is more efficient that SC_REPORT_INFO_VERB because it
+// does not evaluate the arguments until and unless the verbosity level
+// is appropriate.
+//
+// Note: SC_REPORT_WARNING, SC_REPORT_ERROR, and SC_REPORT_FATAL do not
+//       need this because they unconditionally execute anyhow.
+
 // Fix efficiency so that message will be evaluated only if verbosity meets requirements
-#ifdef  SC_REPORT_INFO_VERB
-  #define REPORT_INFO_VERB(mesgType,message,verbosity) do {\
-    if( verbosity <= ::sc_core::sc_report_handler::get_verbosity_level() ) \
-      SC_REPORT_INFO_VERB(mesgType,message,verbosity);\
-  } while(false)
-#else
-  #error "Must include SystemC before " __FILE__
-#endif
+#define REPORT_INFO_VERB(msg_type,message,level) do {\
+  if( level <= ::sc_core::sc_report_handler::get_verbosity_level() ) \
+    SC_REPORT_INFO_VERB(msg_type,message,level);\
+} while(false)
 
 // Macro to support std::format or fmt::format if present
 #if __cplus_plus__ > 202003ul
-  #define REPORT_VERB_FORMAT(mesgType,verbosity,fmt,...) do {\
-    if( verbosity <= ::sc_core::sc_report_handler::get_verbosity_level() ) \
-      SC_REPORT_INFO_VERB(mesgType,::std::format(fmt,__VA_ARGS__).c_str(),verbosity);\
+  #define REPORT_VERB_FORMAT(msg_type,level,fmt,...) do {\
+    if( level <= ::sc_core::sc_report_handler::get_verbosity_level() ) \
+      SC_REPORT_INFO_VERB(msg_type,::std::format(fmt,__VA_ARGS__).c_str(),level);\
   } while(false)
 #else
   #ifdef FMT_FORMAT_H_
-    #define REPORT_VERB_FORMAT(mesgType,verbosity,fmt,...) do {\
-      if( verbosity <= ::sc_core::sc_report_handler::get_verbosity_level() ) \
-        SC_REPORT_INFO_VERB(mesgType,::fmt::format(fmt,__VA_ARGS__).c_str(),verbosity);\
+    #define REPORT_VERB_FORMAT(msg_type,level,fmt,...) do {\
+      if( level <= ::sc_core::sc_report_handler::get_verbosity_level() ) \
+        SC_REPORT_INFO_VERB(msg_type,::fmt::format(fmt,__VA_ARGS__).c_str(),level);\
     } while(false)
   #else
-    #define REPORT_VERB_FORMAT(mesgType,verbosity,fmt,...) do {\
+    #define REPORT_VERB_FORMAT(msg_type,level,fmt,...) do {\
        SC_REPORT_ERROR( "/Doulos/reporting_enhancement", "Missing support for REPORT_VERB_FORMAT" );\
-       SC_REPORT_INFO_VERB( mesgType, fmt, verbosity );\
+       SC_REPORT_INFO_VERB( msg_type, fmt, level );\
     } while(false)
   #endif
 #endif
@@ -39,8 +42,8 @@
   #define COLOR_STR(cstr) ""
 #endif
 
-#define COLOR_BOLD    std::string{ COLOR_STR( "\033[0m"  ) }
-#define COLOR_NONE    std::string{ COLOR_STR( "\033[1m"  ) }
+#define COLOR_BOLD    std::string{ COLOR_STR( "\033[1m"  ) }
+#define COLOR_NONE    std::string{ COLOR_STR( "\033[0m"  ) }
 #define COLOR_BLACK   std::string{ COLOR_STR( "\033[90m" ) }
 #define COLOR_RED     std::string{ COLOR_STR( "\033[91m" ) }
 #define COLOR_GREEN   std::string{ COLOR_STR( "\033[92m" ) }
@@ -69,8 +72,8 @@
 #define REPORT_ALWAYS(mesg)     REPORT_INFO_VERB(  Doulos::text(msg_type), Doulos::text(mesg, ::sc_core::SC_INFO, ::sc_core::SC_NONE ), ::sc_core::SC_NONE )
 #define REPORT_DEBUG(mesg)      REPORT_INFO_VERB(  Doulos::text(msg_type),\
   Doulos::text( std::string{"Debug: "} + std::string{mesg} \
-  + "\nFile:"s + std::string{__FILE__} + " Line:"s + std::to_string(__LINE__)\
-  + " at "s + ::sc_core::sc_time_stamp().to_string()\
+  + std::string{"\nFile:"} + std::string{__FILE__} + std::string{" Line:"} + std::to_string(__LINE__)\
+  + std::string{" at "} + ::sc_core::sc_time_stamp().to_string()\
   , ::sc_core::SC_INFO, ::sc_core::SC_DEBUG), ::sc_core::SC_DEBUG )
 #define REPORT_NUM(var) REPORT_DEBUG( std::string{#var} + std::string{"="} + std::to_string(var) )
 #define REPORT_STR(var) REPORT_DEBUG( std::string{#var} + std::string{"="} + var                 )
@@ -78,23 +81,23 @@
 
 namespace Doulos {
 // Convert a string to const char*
-static inline const char* text( const std::string& s ) {
-  return s.c_str();
+static inline const char* text( const std::string& the_string ) {
+  return the_string.c_str();
 }
 
-static inline const char* text( const std::string& s
+static inline const char* text( const std::string& the_string
                        , ::sc_core::sc_severity severity
-                       , int verbosity = ::sc_core::SC_MEDIUM ) {
+                       , int level = ::sc_core::SC_MEDIUM ) {
   static std::string result{};
   result.clear();
   switch( severity ) {
-    case ::sc_core::SC_INFO:    result += (verbosity >= ::sc_core::SC_DEBUG)? COLOR_DEBUG : COLOR_INFO; break;
+    case ::sc_core::SC_INFO:    result += (level >= ::sc_core::SC_DEBUG)? COLOR_DEBUG : COLOR_INFO; break;
     case ::sc_core::SC_WARNING: result += COLOR_WARN;  break;
     case ::sc_core::SC_ERROR:   result += COLOR_ERROR; break;
     case ::sc_core::SC_FATAL:   result += COLOR_FATAL; break;
     default: break;
   }
-  result += s + COLOR_NONE;
+  result += the_string + COLOR_NONE;
   return result.c_str();
 }
 
